1390db05e91fa62c421b44086e53ff6f68e7501e
pulgamecanica <pulgamecanica11@gmail.com>
2021-06-03 22:34:56 +0100

    Fix image off box in home

--- app/controllers/visitors/home_controller.rb
+++ app/controllers/visitors/home_controller.rb
@@ -2,7 +2,7 @@ module Visitors
 	class HomeController < VisitorsController
 	  def index
 	  	@categories = Category.active_categories.order_by_rank
-	  	@posts = Post.where(active: true).order(:updated_at).limit(1)
+	  	@posts = Post.where(active: true).order(:created_at).limit(1)
 	  end
 	  def info
 	  	@posts = Post.where(active: true)
