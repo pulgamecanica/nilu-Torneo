94f23d7a738b2c6271dfe7aaf720a5dae6ef908c
pulgamecanica <pulgamecanica11@gmail.com>
2021-06-15 09:31:35 +0100

    Order by name and show winners on category page

--- app/controllers/visitors/categories_controller.rb
+++ app/controllers/visitors/categories_controller.rb
@@ -3,13 +3,12 @@ module Visitors
     before_action :visitor_get_random_background
 		def show
 			@category = Category.find(params[:id])
-			@players = Player.where(category_id: params[:id]).order(:rank)
+			@players = Player.where(category_id: params[:id]).order(:name)
 			@matches = Match.where(category_id: params[:id])
 		end
 		def visitor_search_players
       @players = Player.where(category_id: params[:category_id]).where("lower(name) LIKE ?", "%#{params[:search].downcase}%").order(:rank)
 		end
-
 		def visitor_search_matches
 			rounds = params[:match][:rounds_filter].reject { |r| r.empty? }
       @matches = Match.where(category_id: params[:category_id])
